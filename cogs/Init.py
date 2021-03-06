ee8c03d266421574fb6f9acc4edc93f1cc00b05d
Lemon Root <thelemonroot@gmail.com>
2021-03-06 00:57:22 -0500

    Ring logic, hints, fortune teller names
    Lots of little things here.

--- cogs/Init.py
+++ cogs/Init.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import pymongo
 from pymongo import MongoClient
 
 mongo_url = "mongodb+srv://lemonroot:LFijfLSGFtxylftV0uUX@cluster0.5jfol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -17,6 +16,17 @@ class Init(commands.Cog):
     async def on_ready(self):
         game = discord.Game('with chao!')
         await self.bot.change_presence(status=discord.Status.online, activity=game)
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if not ctx.author.bot:
+            users = db["users"]
+            myquery = {"_id": ctx.author.id}
+            search = users.count_documents(myquery)
+            if search != 0:
+                users.update_one({"_id": ctx.author.id}, {"$inc": {"rings": 1}})
+            else:
+                return
 
 
 def setup(bot):
